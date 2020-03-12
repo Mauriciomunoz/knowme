ae94198ede85b62e3d0b881aa359ac04f6d7e504
Miguel Munoz <Miguel.munoz@ihg.com>
2020-03-11 22:05:43 -0600

    Function for movies added

--- app/src/main/java/com/mmsoft/knowme/ui/mainknowme/MainKnowMeFragment.kt
+++ app/src/main/java/com/mmsoft/knowme/ui/mainknowme/MainKnowMeFragment.kt
@@ -7,15 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.ListFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.mmsoft.knowme.MoviesTop
 import com.mmsoft.knowme.R
 import com.mmsoft.knowme.adapter.CategoryAdapter
 import com.mmsoft.knowme.databinding.CategoryItemBinding
 import com.mmsoft.knowme.databinding.MainKnowMeFragmentBinding
+import com.mmsoft.knowme.ui.mytop.MyTopFragment
 import com.mmsoft.knowme.viewmodel.CategoryViewModel
 import kotlinx.android.synthetic.main.main_know_me_fragment.*
 
@@ -26,19 +30,19 @@ class MainKnowMeFragment : Fragment() {
     }
 
     private lateinit var viewModel: CategoryViewModel
-    private lateinit var binding:MainKnowMeFragmentBinding
+    private lateinit var binding: MainKnowMeFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding=MainKnowMeFragmentBinding.inflate(inflater,container,false)
-
         binding.btnMovies.setOnClickListener{
-
+            navigateToCategory()
         }
 
-        return inflater.inflate(R.layout.main_know_me_fragment, container, false)
+        return binding.root
+        //return inflater.inflate(R.layout.main_know_me_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -58,17 +62,21 @@ class MainKnowMeFragment : Fragment() {
     }
 
     private fun navigateToCategory(){
+
+        //Toast.makeText(this.context, "Hi there! This is a Toast.", Toast.LENGTH_SHORT).show()
         // Create new fragment (Either map or list fragment) and the transaction
-        val listFragment = ListFragment()
+        val fragment = MoviesTop()
         val transaction = fragmentManager!!.beginTransaction()
+
 
         // Replace whatever is in the fragment_container view with this fragment,
         // and add the transaction to the back stack if needed
-        transaction.replace(R.id.myTopFragment, listFragment)
-        transaction.addToBackStack(null)
+        transaction.replace(R.id.mainknowme, fragment)
+  //      transaction.addToBackStack(null)
 
         // Commit the transaction
         transaction.commit()
+
     }
 
 }
